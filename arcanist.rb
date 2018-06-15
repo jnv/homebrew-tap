19f5adb4da56c689cc87ed9f10e0e7a09a3d313e
Jan Vlnas <git@jan.vlnas.cz>
2018-06-15 13:06:07 +0200

    Add HEAD formula for arcanist

---
+++ arcanist.rb
@@ -0,0 +1,31 @@
+class Arcanist < Formula
+  desc "Phabricator Arcanist Tool"
+  homepage "https://secure.phabricator.com/book/phabricator/article/arcanist/"
+
+  depends_on "php"
+
+  head "https://github.com/phacility/arcanist.git"
+  resource "libphutil" do
+    url "https://github.com/phacility/libphutil.git"
+  end
+
+  def install
+    libexec.install Dir["*"]
+
+    resource("libphutil").stage do
+      (buildpath/"libphutil").install Dir["*"]
+    end
+
+    prefix.install Dir["*"]
+
+    bin.install_symlink libexec/"bin/arc" => "arc"
+
+    cp libexec/"resources/shell/bash-completion", libexec/"resources/shell/arc-completion.zsh"
+    bash_completion.install libexec/"resources/shell/bash-completion" => "arc"
+    zsh_completion.install libexec/"resources/shell/arc-completion.zsh" => "_arc"
+  end
+
+  test do
+    system "#{bin}/arc", "help"
+  end
+end
